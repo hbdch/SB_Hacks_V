249c712f88249b71465a9d83fe2f25288e462b94
DKW2 <32755558+DKW2@users.noreply.github.com>
2019-01-12 17:13:48 -0800

    Add files via upload

---
+++ personalityComparison.py
@@ -0,0 +1,43 @@
+# Takes a person's personality values and compare it to a dictionary of company personality values.
+# Dictionary wit keys as company names
+# Creates a file called results.json in the data folder with an array of the top 5 company names
+import json
+import operator
+
+def loadJSONData(filename):
+	data = open(filename).read()
+	return json.loads(data)
+
+def compareData(traits, companies):
+	companyScores = {}	
+	for company in companies.keys():
+		score = 0
+		for trait in traits.keys():
+			score += (companies[company][trait] - traits[trait])**2
+		companyScores[company] = score
+	sortedScores = sorted(companyScores.items(), key = operator.itemgetter(1))
+	print(sortedScores)
+	print(sortedScores[0][0] + " is the best company that fits your personality!")
+	return sortedScores
+
+def writeToJSONFile(fileName, data):
+    filePathNameWExt = 'data/' + fileName + '.json'
+    with open(filePathNameWExt, 'w') as fp:
+        json.dump(data, fp)
+
+def saveResults(results):
+	topFive = []
+	for i in range(0,5):
+		if i >= len(results):
+			break
+		topFive.append(results[i][0])
+	writeToJSONFile("results", topFive)
+
+def main():
+	personalityTraits = loadJSONData("data/values.json")
+	companies = loadJSONData("data/companies.json")
+	results = compareData(personalityTraits, companies)
+	saveResults(results)
+
+if __name__ == "__main__":
+	main()
